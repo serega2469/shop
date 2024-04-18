d875c93018dbdc885a20bb64cd73e1cf67b8df7f
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-04-18 18:17:36 +0100

    ДЗ 23.04.2024

--- store/models.py
+++ store/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=30)
+    # TODO: Улушить модель
+    name = models.CharField(max_length=30, verbose_name='Название')
     slug = models.SlugField()
+
+
+# TODO: Реализовать модель Product
+# TODO: Использовать ForeignKey Product и Category
