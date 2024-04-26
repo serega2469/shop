66c0e565c8e1e6a3292998071e0d6e44377cc4ff
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-04-26 13:50:17 +0100

    Разбор ДЗ
    - Улучшена модель `Category`
     - Реализация админ-панели

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from store.models import Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', )
+    readonly_fields = ('created_at',)
+    fields = ('name', 'slug', 'created_at')
+
+
+# TODO: Написать настройки админ-панели для Product
+
+admin.site.register(Category, CategoryAdmin)
--- store/models.py
+++ store/models.py
@@ -1,19 +1,39 @@
 from django.db import models
-from django.utils import timezone
+
+from store.constants import (
+    CATEGORY_NAME_LEN,
+    CATEGORY_SLUG_LEN,
+)
 
 
 class Category(models.Model):
-    # TODO: Улушить модель
-    name = models.CharField(max_length=30, verbose_name='Название')
-    slug = models.SlugField()
-    created_at = models.DateTimeField(default=timezone.now)
+    name = models.CharField(
+        'Название',
+        max_length=CATEGORY_NAME_LEN,
+    )
+    slug = models.SlugField(
+        max_length=CATEGORY_SLUG_LEN,
+        verbose_name='SLUG',
+        help_text='slug только из английских букв'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания'
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('-created_at',)
 
 
-# TODO: Реализовать модель Product
-# TODO: Использовать ForeignKey Product и Category
+# TODO: 1. Улучшить модель,
+#  2. написать verbose_name, указать максимальную длину
+#  3. Написать Meta класс
 class Product(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
-    price = models.DecimalField()
+    price = models.PositiveIntegerField()
     quantity = models.PositiveIntegerField()
     category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
+    is_available = models.BooleanField('В наличии', default=True)
