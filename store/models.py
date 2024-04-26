f7d5528252065aeae247cb1fba379e9fffde2da6
Serega <cergey.pozdnyakov.99@mail.ru>
2024-04-26 14:29:40 +0300

    Работа над моделями
    - Модель Product
    - Test text

--- store/models.py
+++ store/models.py
@@ -1,11 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Category(models.Model):
     # TODO: Улушить модель
     name = models.CharField(max_length=30, verbose_name='Название')
     slug = models.SlugField()
+    created_at = models.DateTimeField(default=timezone.now)
 
 
 # TODO: Реализовать модель Product
 # TODO: Использовать ForeignKey Product и Category
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    price = models.DecimalField()
+    quantity = models.PositiveIntegerField()
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
