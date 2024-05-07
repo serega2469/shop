9beaaa6a16554f5057f52739095c0af2286a6153
Serega <cergey.pozdnyakov.99@mail.ru>
2024-05-07 14:43:00 +0300

    admin, models

--- store/admin.py
+++ store/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
 
-from store.models import Category
+from store.models import Category, Product
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug', )
+    list_display = ('name', 'slug')
     readonly_fields = ('created_at',)
     fields = ('name', 'slug', 'created_at')
 
 
 # TODO: Написать настройки админ-панели для Product
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category', 'is_available')
+    # readonly_fields = ('created_at',)
+    fields = ('name', 'quantity', 'category', 'price')
+
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+
--- store/models.py
+++ store/models.py
@@ -26,6 +26,9 @@ class Category(models.Model):
         verbose_name_plural = 'Категории'
         ordering = ('-created_at',)
 
+    def __str__(self):
+        return self.name
+
 
 # TODO: 1. Улучшить модель,
 #  2. написать verbose_name, указать максимальную длину
@@ -33,7 +36,19 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
+    # created_at = models.DateTimeField(
+    #     auto_now_add=True,
+    #     verbose_name='Дата создания'
+    # )
     price = models.PositiveIntegerField()
     quantity = models.PositiveIntegerField()
     category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
     is_available = models.BooleanField('В наличии', default=True)
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ('price',)
+
+    def __str__(self):
+        return self.name
